1ceee4d22266c41a21935073e48997f80d957662
Rafael Assis Santos <101332916+professor-rafael@users.noreply.github.com>
2024-05-18 23:51:59 -0300

    Add files via upload

--- p5.collide2d.js
+++ p5.collide2d.js
@@ -1,13 +1,9 @@
 /*
-Repo: https://github.com/bmoren/p5.collide2D/
-Created by http://benmoren.com
-Some functions and code modified version from http://www.jeffreythompson.org/collision-detection
-Version v0.7.3 | June 22, 2020
-CC BY-NC-SA 4.0
+
 */
 
 
-console.log("Prof.Rafael")
+console.log("professor-rafael")
 
 p5.prototype._collideDebug = false;
 
